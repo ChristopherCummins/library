a283b3028854cb33633817c64946f4fbe2437a3c
Christopher Cummins <christopher.francis.cummins@gmail.com>
2021-07-18 11:00:00 -0400

    books are displayed

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ function Book (title, author, numPages, read) {
 
 let Bible = new Book("The Bible", "God", 1700, true);
 
+let Hobbit = new Book("The Hobbit", "JRR Tolkein", 3443, true);
+
+
 myLibrary.push(Bible);
+myLibrary.push(Hobbit);
 
 console.log(myLibrary);
 
@@ -20,46 +24,73 @@ function showForm() {
 form = document.querySelector("form");
 function addBookToLibrary(form) {
     let newBook = new Book (form.title.value, form.author.value, form.numPages.value, form.read.value);
-    let bookArray = [newBook.Title, newBook.Author, newBook.Pages, newBook.Read];
     myLibrary.push(newBook);
     console.log(myLibrary);
-    addBookToTable(table, bookArray);
+
+    document.getElementById("addBookForm").reset();
 
 }
 
-function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
-}
+function newElement() {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let numPages = document.getElementById("numPages").value;
+    let read = document.getElementById("read").value;
 
-function addBookToTable(table, bookArray) {
-    let row = table.insertRow();
-    for (let key of bookArray) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(key);
-        cell.appendChild(text);
-    }
-}
+    let heading = document.createElement("h3");
+    heading.classList.add("heading");
+    let t = document.createTextNode(title);
+    heading.appendChild(t);
+    let subheader = document.createElement("h4");
+    let a = document.createTextNode(author);
+    subheader.appendChild(a);
+    let p1 = document.createElement("p");
+    let n = document.createTextNode(numPages);
+    p1.appendChild(n);
+    let p2 = document.createElement("p");
+    let r = document.createTextNode(read);
+    p2.appendChild(r);
 
-function generateTable(table, data) {
-    for (let element of data) {
-        let row = table.insertRow();
-        for (key in element) {
-            let cell = row.insertCell();
-            let text = document.createTextNode(element[key]);
-            cell.appendChild(text);
+    card.appendChild(heading);
+    card.appendChild(subheader);
+    card.appendChild(p1);
+    card.appendChild(p2);
+
+    document.getElementById("cardContainer").appendChild(card);
+
+    addBookToLibrary(form);
+
+    let span = document.createElement("SPAN");
+    let remove = document.createTextNode("Remove");
+    span.className = "remove";
+    span.appendChild(remove);
+    card.appendChild(span);
+
+    let removeList = document.getElementsByClassName("remove");
+    for (i = 0; i < removeList.length; i++) {
+        removeList[i].onclick = function() {
+            let div = this.parentElement;
+            div.remove();
         }
     }
+
+}   
+
+let numBooks = document.getElementsByClassName("card")
+for (i = 0; i < numBooks.length; i++) {
+    let span = document.createElement("SPAN");
+    let remove = document.createTextNode("Remove");
+    span.className = "remove";
+    span.appendChild(remove);
+    numBooks[i].appendChild(span);
 }
 
-let table = document.querySelector("table");
-let data = Object.keys(myLibrary[0]);
-generateTable(table, myLibrary);
-generateTableHead(table, data);
-
+let remove = document.getElementsByClassName("remove");
+for (i = 0; i < remove.length; i++) {
+    remove[i].onclick = function() {
+        let div = this.parentElement;
+        div.remove();
+    }
+}
