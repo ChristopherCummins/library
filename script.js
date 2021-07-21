e7b7bfbfa1ac881f0b3a0b804fa06565316f5625
Christopher Cummins <christopher.francis.cummins@gmail.com>
2021-07-21 15:33:29 -0400

    updates

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function newElement() {
     clearCards();
     showCards();
     addRemoveButton();
+    addUpdateButton();
 }
 
 function showCards() {
@@ -55,15 +56,15 @@ function createCard() {
     heading.appendChild(t);
     let subheader = document.createElement("h4");
     subheader.classList.add("subheader");
-    let a = document.createTextNode("by " + author);
+    let a = document.createTextNode("Author: " + author);
     subheader.appendChild(a);
     let p1 = document.createElement("p");
     p1.classList.add("numPages")
-    let n = document.createTextNode(numPages + " pages");
+    let n = document.createTextNode("Page count: " + numPages);
     p1.appendChild(n);
     let p2 = document.createElement("p");
     p2.classList.add("readStatus");
-    let r = document.createTextNode(read);
+    let r = document.createTextNode("Status: " + read);
     p2.appendChild(r);
 
     card.appendChild(heading);
@@ -73,11 +74,18 @@ function createCard() {
 
     document.getElementById("cardContainer").appendChild(card); 
     
+    let updateButton = document.createElement("button");
+    let update = document.createTextNode("Update Status");
+    updateButton.className = "btn btn-primary";
+    updateButton.appendChild(update);
+    card.appendChild(updateButton);
+
     let removeButton = document.createElement("button");
     let remove = document.createTextNode("Remove");
     removeButton.className = "btn btn-danger";
     removeButton.appendChild(remove);
     card.appendChild(removeButton);
+
 }
 
 function clearCards() {
@@ -92,79 +100,29 @@ function addRemoveButton() {
             let div = this.parentElement;
             div.remove();
             myLibrary.splice(remove[i], 1);
+            checkLibrary();
         }
     }
 }
 
 addRemoveButton();
 
-/* 
-function newElement() {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let numPages = document.getElementById("numPages").value;
-    let read = document.getElementById("read").value;
-
-    let heading = document.createElement("h3");
-    heading.classList.add("heading");
-    let t = document.createTextNode(title);
-    heading.appendChild(t);
-    let subheader = document.createElement("h4");
-    subheader.classList.add("subheader");
-    let a = document.createTextNode("by " + author);
-    subheader.appendChild(a);
-    let p1 = document.createElement("p");
-    p1.classList.add("numPages")
-    let n = document.createTextNode(numPages + " pages");
-    p1.appendChild(n);
-    let p2 = document.createElement("p");
-    p2.classList.add("readStatus");
-    let r = document.createTextNode(read);
-    p2.appendChild(r);
-
-    card.appendChild(heading);
-    card.appendChild(subheader);
-    card.appendChild(p1);
-    card.appendChild(p2);
-
-    document.getElementById("cardContainer").appendChild(card);
-
-    addBookToLibrary(form);
-    document.getElementById("addBookForm").reset();
-
-    let removeButton = document.createElement("button");
-    let remove = document.createTextNode("Remove");
-    removeButton.className = "btn btn-danger";
-    removeButton.appendChild(remove);
-    card.appendChild(removeButton);
-
-    let removeList = document.getElementsByClassName("btn btn-danger");
-    for (i = 0; i < removeList.length; i++) {
-        removeList[i].onclick = function() {
-            let div = this.parentElement;
-            div.remove();
+function addUpdateButton() {
+    let update = document.getElementsByClassName("btn btn-primary");
+    for (i = 0; i < update.length; i++) {
+        update[i].onclick = function() {
+        myLibrary[i].Read = "Read";
         }
     }
-    
-}   
-
-let numBooks = document.getElementsByClassName("card")
-for (i = 0; i < numBooks.length; i++) {
-    let removeButton = document.createElement("button");
-    let remove = document.createTextNode("Remove");
-    removeButton.className = "btn btn-danger";
-    removeButton.appendChild(remove);
-    numBooks[i].appendChild(removeButton);
 }
 
-let remove = document.getElementsByClassName("btn btn-danger");
-for (i = 0; i < remove.length; i++) {
-    remove[i].onclick = function() {
-        let div = this.parentElement;
-        div.remove();
+addUpdateButton();
+
+function checkLibrary() {
+    if (myLibrary.length === 0) {
+        let cardContainer = document.getElementById("cardContainer");
+        cardContainer.innerHTML ="Your library is empty. Add a book!";
     }
 }
 
- */
+checkLibrary();
